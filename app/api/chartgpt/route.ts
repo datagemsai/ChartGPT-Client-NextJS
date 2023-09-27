ddfd7f071a99e8ca64131cb6447f1eb32a25d7a1
BenSchZA <BenSchZA@users.noreply.github.com>
2023-09-27 18:15:33 +0200

    Remove Sentry

--- app/api/chartgpt/route.ts
+++ app/api/chartgpt/route.ts
@@ -72,6 +72,49 @@ function parseChartGPTResult(result: any): string {
 }
 
 
+async function onCompletion(
+  json: any,
+  messages: any,
+  userId: string,
+  dataSourceURL: string,
+  completion: String
+) {
+  const title = json.messages[0].content.substring(0, 100)
+  const id = json.id ?? nanoid()
+  const createdAt = Date.now()
+  const path = `/chat/${id}`
+  const payload = {
+    id,
+    title,
+    userId,
+    createdAt,
+    path,
+    dataSourceURL,
+    // messages: [
+    //   ...messages,
+    //   {
+    //     content: completion,
+    //     role: 'assistant',
+    //   }
+    // ],
+  }
+  await kv.hmset(`chat:${id}`, payload)
+  const messages_exist = await kv.exists(`chat:${id}:messages`)
+  if (messages_exist) {
+    await kv.rpush(`chat:${id}:messages`, JSON.stringify({
+      content: completion,
+      role: 'assistant',
+    }))
+  } else {
+    await messages.map((message: any) => kv.rpush(`chat:${id}:messages`, JSON.stringify(message)))
+  }
+  await kv.zadd(`user:chat:${userId}`, {
+    score: createdAt,
+    member: `chat:${id}`
+  })
+}
+
+
 export async function POST(req: Request): Promise<Response> {
   const json = await req.json()
   const { messages, previewToken, dataSourceURL } = json
@@ -85,103 +128,81 @@ export async function POST(req: Request): Promise<Response> {
 
   const user_messages = messages.filter((message: any) => message.role === 'user')
 
-  const response = await fetch(`${process.env.CHARTGPT_API_HOST}/v1/ask_chartgpt/stream`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'X-API-KEY': process.env.CHARTGPT_API_KEY ?? '',
-    },
-    body: JSON.stringify({
-      messages: user_messages,
-      data_source_url: dataSourceURL,
-      output_type: process.env.CHARTGPT_OUTPUT_TYPE,
+  try {
+    const response = await fetch(`${process.env.CHARTGPT_API_HOST}/v1/ask_chartgpt/stream`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'X-API-KEY': process.env.CHARTGPT_API_KEY ?? '',
+      },
+      body: JSON.stringify({
+        messages: user_messages,
+        data_source_url: dataSourceURL,
+        output_type: process.env.CHARTGPT_OUTPUT_TYPE,
+      })
     })
-  })
 
-  if (!response.body || !response.ok) {
-    return new Response('API error', {
-      status: 500
-    })
-  }
-
-  const encoder = new TextEncoder()
-  const decoder = new TextDecoder()
-
-  async function onCompletion(completion: String) {
-    const title = json.messages[0].content.substring(0, 100)
-    const id = json.id ?? nanoid()
-    const createdAt = Date.now()
-    const path = `/chat/${id}`
-    const payload = {
-      id,
-      title,
-      userId,
-      createdAt,
-      path,
-      dataSourceURL,
-      // messages: [
-      //   ...messages,
-      //   {
-      //     content: completion,
-      //     role: 'assistant',
-      //   }
-      // ],
+    if (!response.body || !response.ok) {
+      console.error(response)
+      return new Response('API error', {
+        status: 500
+      })
     }
-    await kv.hmset(`chat:${id}`, payload)
-    const messages_exist = await kv.exists(`chat:${id}:messages`)
-    if (messages_exist) {
-      await kv.rpush(`chat:${id}:messages`, JSON.stringify({
-        content: completion,
-        role: 'assistant',
-      }))
-    } else {
-      await messages.map((message: any) => kv.rpush(`chat:${id}:messages`, JSON.stringify(message)))
-    }
-    await kv.zadd(`user:chat:${userId}`, {
-      score: createdAt,
-      member: `chat:${id}`
-    })
-  }
 
-  let counter = 0
-  let completion = ''
+    const encoder = new TextEncoder()
+    const decoder = new TextDecoder()
 
-  const stream = new ReadableStream({
-    async start(controller) {
-      async function onParse(event: ParsedEvent | ReconnectInterval): Promise<void> {
-        if (event.type === "event") {
-          const data = event.data;
-          if (data === "[DONE]") {
-            await onCompletion(completion);
-            controller.close();
-            return;
-          }
-          try {
-            const result = JSON.parse(data);
-            const output_value = parseChartGPTResult(result)
-            const queue = encoder.encode(output_value);
-            completion += output_value;
-            controller.enqueue(queue);
-            counter++;
-          } catch (e) {
-            controller.error(e);
+    let counter = 0
+    let completion = ''
+
+    const stream = new ReadableStream({
+      async start(controller) {
+        async function onParse(event: ParsedEvent | ReconnectInterval): Promise<void> {
+          if (event.type === "event") {
+            const data = event.data;
+            if (data === "[DONE]") {
+              await onCompletion(
+                json,
+                messages,
+                userId,
+                dataSourceURL,
+                completion,
+              );
+              controller.close();
+              return;
+            }
+            try {
+              const result = JSON.parse(data);
+              const output_value = parseChartGPTResult(result)
+              const queue = encoder.encode(output_value);
+              completion += output_value;
+              controller.enqueue(queue);
+              counter++;
+            } catch (e) {
+              controller.error(e);
+            }
           }
         }
-      }
 
-     // stream response (SSE) may be fragmented into multiple chunks
-     // this ensures we properly read chunks & invoke an event for each SSE event stream
-     const parser = createParser(onParse);
+      // stream response (SSE) may be fragmented into multiple chunks
+      // this ensures we properly read chunks & invoke an event for each SSE event stream
+      const parser = createParser(onParse);
 
-      // https://web.dev/streams/#asynchronous-iteration
-      for await (const chunk of response.body as any) {
-        parser.feed(decoder.decode(chunk));
-      }
-    },
-  });
+        // https://web.dev/streams/#asynchronous-iteration
+        for await (const chunk of response.body as any) {
+          parser.feed(decoder.decode(chunk));
+        }
+      },
+    });
 
-  // return new Response(stream, {
-  //   headers: { 'Content-Type': 'text/html; charset=utf-8' },
-  // })
-  return new StreamingTextResponse(stream)
+    // return new Response(stream, {
+    //   headers: { 'Content-Type': 'text/html; charset=utf-8' },
+    // })
+    return new StreamingTextResponse(stream)
+  } catch (error) {
+    console.error(error)
+    return new Response('API error', {
+      status: 500,
+    })
+  }
 }

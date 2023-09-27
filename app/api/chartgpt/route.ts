cdb8faf804fc48265255160b2a73f6b8516317a3
BenSchZA <BenSchZA@users.noreply.github.com>
2023-09-27 11:07:09 +0200

    Update to streaming architecture

--- app/api/chartgpt/route.ts
+++ app/api/chartgpt/route.ts
@@ -1,17 +1,81 @@
 import { kv } from '@vercel/kv'
 import { OpenAIStream, StreamingTextResponse } from 'ai'
 import { Configuration, OpenAIApi } from 'openai-edge'
+import { createParser, ParsedEvent, ReconnectInterval } from 'eventsource-parser'
 
 import { auth } from '@/auth'
 import { nanoid } from '@/lib/utils'
 import { format } from 'sql-formatter'
 import { log } from 'console'
 import { Message } from 'ai'
+import { output } from '@/next.config'
 
 export const runtime = 'edge'
 
 
-export async function POST(req: Request) {
+function parseChartGPTResult(result: any): string {
+  let output_value = ''
+
+  console.log(`Received result for job ID: ${result.id}`)
+
+  const outputs = result.outputs
+  const attempts = result.attempts
+
+  if (!outputs.length) {
+    for (const attempt of attempts) {
+      if (attempt.outputs.length) {
+        output_value += `I'm thinking... 🤔\n\n`
+      }
+    }
+  }
+
+  if (outputs.length && outputs[0]?.value) {
+    const output = outputs[0]
+    console.log(`${result.id}: Processing output of type ${output.type}`)
+    console.debug(`${result.id}: First 10 characters of output: ${output.value.substring(0, 10)}`)
+    console.debug(`${result.id}: Last 10 characters of output: ${output.value.substring(output.value.length - 10)}`)
+    if (output.type === "sql_query") {
+      output_value += output.description
+      output_value += "\n"
+      output_value += "```sql\n"
+      output_value += format(output.value, { language: 'bigquery' })
+      output_value += "\n```"
+      output_value += "\n"
+    } else if (output.type === "pandas_dataframe") {
+      // output_value += output.description
+      output_value += "\n"
+      output_value += "```table\n"
+      output_value += output.value
+      output_value += "\n```"
+      output_value += "\n"
+    } else if (output.type === "python_code") {
+      output_value += output.description
+      output_value += "\n"
+      output_value += "```python\n"
+      output_value += output.value
+      output_value += "\n```"
+      output_value += "\n"
+    } else if (["string", "int", "float", "bool"].includes(output.type)) {
+      // "python_output"
+      // output_value += "```\n"
+      output_value += output.value.substring(0, 140)
+      // output_value += "\n```"
+      // output_value += "\n"
+    } else if (output.type === "plotly_chart") {
+      output_value += "\n"
+      output_value += "```chart\n"
+      output_value += output.value
+      output_value += "\n```"
+      output_value += "\n"
+    } else {
+      console.log(`${result.id}: Unknown or unhandled output type: ${output.type}`)
+    }
+  }
+  return output_value
+}
+
+
+export async function POST(req: Request): Promise<Response> {
   const json = await req.json()
   const { messages, previewToken, dataSourceURL } = json
   const userId = (await auth())?.user.id
@@ -43,9 +107,8 @@ export async function POST(req: Request) {
     })
   }
 
-  const reader = response.body.getReader()
-  const decoder = new TextDecoder('utf-8')
   const encoder = new TextEncoder()
+  const decoder = new TextDecoder()
 
   async function onCompletion(completion: String) {
     const title = json.messages[0].content.substring(0, 100)
@@ -74,90 +137,42 @@ export async function POST(req: Request) {
     })
   }
 
-  let buffer = ''
+  let counter = 0
   let completion = ''
-  const customStream = new ReadableStream({
-    async pull(controller) {
-      const { done, value } = await reader.read()
 
-      if (done) {
-        await onCompletion(completion)
-        controller.close()
-      } else {
-        const chunk = decoder.decode(value, {stream:true})
-        console.log(`Received ${chunk.length} characters of data`)
-        buffer += chunk
-        let output_value = ""
-
-        if (buffer.includes('<end>\n\n')) {
-          const results = buffer.split('<end>\n\n');
-          buffer = results.pop() || '';
-  
-          for(const raw_result of results){
-            const result = JSON.parse(raw_result.trim().replace(/^data: /, ''))
-            console.log(`${result.id}: Received result of length ${raw_result.length}`)
-
-            const outputs = result.outputs
-            const attempts = result.attempts
-
-            if (!outputs.length) {
-              for (const attempt of attempts) {
-                if (attempt.outputs.length) {
-                  output_value += `I'm thinking... 🤔\n\n`
-                }
-              }
-            }
-
-            if (outputs.length && outputs[0]?.value) {
-              const output = outputs[0]
-              console.log(`${result.id}: Processing output of type ${output.type}`)
-              console.debug(`${result.id}: First 10 characters of output: ${output.value.substring(0, 10)}`)
-              console.debug(`${result.id}: Last 10 characters of output: ${output.value.substring(output.value.length - 10)}`)
-              if (output.type === "sql_query") {
-                output_value += output.description
-                output_value += "\n"
-                output_value += "```sql\n"
-                output_value += format(output.value, { language: 'bigquery' })
-                output_value += "\n```"
-                output_value += "\n"
-              } else if (output.type === "pandas_dataframe") {
-                // output_value += output.description
-                output_value += "\n"
-                output_value += "```table\n"
-                output_value += output.value
-                output_value += "\n```"
-                output_value += "\n"
-              } else if (output.type === "python_code") {
-                output_value += output.description
-                output_value += "\n"
-                output_value += "```python\n"
-                output_value += output.value
-                output_value += "\n```"
-                output_value += "\n"
-              } else if (["string", "int", "float", "bool"].includes(output.type)) {
-                // "python_output"
-                // output_value += "```\n"
-                output_value += output.value.substring(0, 140)
-                // output_value += "\n```"
-                // output_value += "\n"
-              } else if (output.type === "plotly_chart") {
-                output_value += "\n"
-                output_value += "```chart\n"
-                output_value += output.value
-                output_value += "\n```"
-                output_value += "\n"
-              } else {
-                console.log(`${result.id}: Unknown or unhandled output type: ${output.type}`)
-              }
-            }
+  const stream = new ReadableStream({
+    async start(controller) {
+      async function onParse(event: ParsedEvent | ReconnectInterval): Promise<void> {
+        if (event.type === "event") {
+          const data = event.data;
+          if (data === "[DONE]") {
+            await onCompletion(completion);
+            controller.close();
+            return;
+          }
+          try {
+            const result = JSON.parse(data);
+            const output_value = parseChartGPTResult(result)
+            const queue = encoder.encode(output_value);
+            completion += output_value;
+            controller.enqueue(queue);
+            counter++;
+          } catch (e) {
+            controller.error(e);
           }
         }
-        // await onCompletion(output_value)
-        completion += output_value
-        controller.enqueue(encoder.encode(output_value))
       }
-    }
-  })
 
-  return new StreamingTextResponse(customStream)
+     // stream response (SSE) may be fragmented into multiple chunks
+     // this ensures we properly read chunks & invoke an event for each SSE event stream
+     const parser = createParser(onParse);
+
+      // https://web.dev/streams/#asynchronous-iteration
+      for await (const chunk of response.body as any) {
+        parser.feed(decoder.decode(chunk));
+      }
+    },
+  });
+
+  return new Response(stream)
 }

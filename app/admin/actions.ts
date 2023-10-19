b7c0729cabe62612c1f97043a5fe9a9d09f47624
BenSchZA <BenSchZA@users.noreply.github.com>
2023-10-19 16:20:37 +0200

    Remove try/catch wrapper, to remove unexpected failures

--- app/admin/actions.ts
+++ app/admin/actions.ts
@@ -4,24 +4,19 @@ import { Chat } from "@/lib/types";
 
 
 export async function getAllChats(): Promise<Chat[]> {
-  try {
-    const allUserIds = await getAllUserIds();
-    let allChats: Chat[] = [];
+  const allUserIds = await getAllUserIds();
+  let allChats: Chat[] = [];
 
-    for (const userId of allUserIds) {
-      const userChats = await getChats(userId);
-      allChats = [...allChats, ...userChats];
-    }
-
-    allChats.sort((a, b) => {
-      return (a.createdAt > b.createdAt ? -1 : 1)
-    });
-
-    return allChats;
-  } catch (error) {
-    console.error(error);
-    return [];
+  for (const userId of allUserIds) {
+    const userChats = await getChats(userId);
+    allChats = [...allChats, ...userChats];
   }
+
+  allChats.sort((a, b) => {
+    return (a.createdAt > b.createdAt ? -1 : 1)
+  });
+
+  return allChats;
 }
 
 export async function getAllUserIds(): Promise<string[]> {

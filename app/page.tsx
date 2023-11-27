e27c9617022d85e5cd88ab185d97f5171e64b904
BenSchZA <BenSchZA@users.noreply.github.com>
2023-11-27 11:58:36 +0200

    Replace DataGems with ChartGPT until logo is confirmed

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import { ChartGalleryRow } from '@/components/chart-gallery'
 import { DataSources } from '@/components/data-sources'
+import config from '@/lib/config'
 
 // export const runtime = 'edge'
 
@@ -7,7 +8,10 @@ export default function IndexPage() {
   return (
     <div>
       <div className="flex h-fit w-full flex-col items-center justify-center p-8 md:my-40">
-        <h1 className="px-2 text-6xl font-bold text-black">Welcome to Data Gems 💎</h1>
+        {/* <h1 className="px-2 text-6xl font-bold text-black">ChartGPT</h1> */}
+        <a href="/">
+          <img src={config.headerLogo} alt="Logo" className="mx-auto hidden h-40 md:block" />
+        </a>
         <p className="mt-3 px-2 text-2xl text-black">Discover insights using data-driven conversations.</p>
         <div className="mt-12 flex w-full flex-col items-center justify-center">
           <a href="/chat" className="rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700">

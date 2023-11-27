771b987e29ae1505b3ad12667c814c06a62d11b1
BenSchZA <BenSchZA@users.noreply.github.com>
2023-11-27 17:03:01 +0200

    Fix layout of ChartGPT logo on small screens

--- app/page.tsx
+++ app/page.tsx
@@ -8,10 +8,8 @@ export default function IndexPage() {
   return (
     <div>
       <div className="flex h-fit w-full flex-col items-center justify-center p-8 md:my-40">
-        {/* <h1 className="px-2 text-6xl font-bold text-black">ChartGPT</h1> */}
-        <a href="/">
-          <img src={config.headerLogo} alt="Logo" className="mx-auto hidden h-40 md:block" />
-        </a>
+        <h1 className="px-2 text-6xl font-bold text-black justif-left sm:hidden block">ChartGPT</h1>
+        <img src={config.headerLogo} alt="Logo" className="mx-auto hidden h-40 sm:block" />
         <p className="mt-3 px-2 text-2xl text-black">Discover insights using data-driven conversations.</p>
         <div className="mt-12 flex w-full flex-col items-center justify-center">
           <a href="/chat" className="rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700">
